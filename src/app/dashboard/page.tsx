10c4bd0f83133c229e0d9e9a94e825d60556e320
Olaf van der Kaa <olaf@ojkaas.nl>
2024-02-29 17:36:43 +0100

    fix die shit!

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import Dashboard from "@/components/Dashboard";
 
 export default function SurveyDashboard() {
+  return 
+  /*
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <Dashboard />
     </div>
-  );
+  );*/
 }
--- src/app/datatables/page.tsx
+++ src/app/datatables/page.tsx
@@ -1,9 +1,11 @@
 import DashboardDatatables from "@/components/DashboardDatatables";
 
 export default function SurveyDatatables() {
+  return
+  /*
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <DashboardDatatables />
     </div>
-  );
+  );*/
 }
