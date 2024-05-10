937a8566081a8f175f3a8a0b77ae4e60453fe1f1
Olaf van der Kaa <olaf@ojkaas.nl>
2024-05-10 17:47:00 +0200

    ....

--- src/lib/mailer/mail.transporter.ts
+++ src/lib/mailer/mail.transporter.ts
@@ -1,7 +1,6 @@
 import { readFileSync } from 'fs'
 import Handlebars from 'handlebars'
 import { createTransport } from 'nodemailer'
-import path from 'path'
 
 export const mailer = createTransport({
   host: process.env.EMAIL_SERVER_HOST,
@@ -22,11 +21,13 @@ type SendEmailOptions = {
   templateData: { [key: string]: any }
 }
 export const sendMail = (options: SendEmailOptions) => {
-  let templatePath = path.resolve('handlebar/invite.hbs')
-  if (options.templateName === 'invite.hbs') templatePath = path.resolve('handlebar/invite.hbs')
+  //let templatePath = path.resolve('handlebar/invite.hbs')
 
-  const emailTemplateSource = readFileSync(templatePath, 'utf8')
+  //if (options.templateName === 'invite.hbs') templatePath = path.resolve('handlebar/invite.hbs')
+
+  //const emailTemplateSource = readFileSync(templatePath, 'utf8')
   //const emailTemplateSource = readFileSync(path.join(__dirname, '../../../handlebar/' + options.templateName), 'utf8')
+  const emailTemplateSource = readFileSync(process.cwd() + 'src/handlebar/invite.hbs', 'utf8')
   const template = Handlebars.compile(emailTemplateSource)
   const html = template(options.templateData)
 
