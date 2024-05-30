48ef1b3a40a45cdddb478d4de62d0e6934da9562
Olaf van der Kaa <olaf@ojkaas.nl>
2024-05-30 15:18:43 +0200

    remove sticky header

--- src/components/shared/link-header.tsx
+++ src/components/shared/link-header.tsx
@@ -17,7 +17,7 @@ type Props = {
 
 const LinkHeader = ({ Links }: Props) => {
   return (
-    <header className='sticky top-0 flex h-16 items-center gap-4 border-b bg-gray-900 px-4 md:px-6'>
+    <header className='top-0 flex h-16 items-center gap-4 border-b bg-gray-900 px-4 md:px-6'>
       <nav className='hidden flex-col gap-6 text-lg font-medium text-gray-400 md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6'>
         <Link href='#'>
           <Image src={'/oogned.png'} priority={true} className='h-auto max-w-screen-md' alt='Oogned logo' width={200} height={50} />
